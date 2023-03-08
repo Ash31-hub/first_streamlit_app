4fa7095ee6aa917027ecf7ef0a1bcd03ba5daa19
Ash31-hub <126770345+Ash31-hub@users.noreply.github.com>
2023-03-09 02:23:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ def get_fruity_vice_data(this_fruit_choice):
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice')
 try:
- fruit_choice = streamlit.text_input('What fruit would you like information about?')
+      fruit_choice = streamlit.text_input('What fruit would you like information about?')
  if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
  else:
@@ -54,7 +54,7 @@ if streamlit.button('Get Fruit Load List'):
 #dont run anything past here while we troubleshoot
 
 
-#Allow the end user to add a fruit to the list
+ #Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
       with my_cnx.cursor()as my_cur:
             my_cur.execute("insert into fruit_load_list values ('from streamlit')")

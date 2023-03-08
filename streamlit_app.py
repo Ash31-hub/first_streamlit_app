8374a0b724478705382fb429798c1f9ea70b0433
Ash31-hub <126770345+Ash31-hub@users.noreply.github.com>
2023-03-08 20:09:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice')
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
  streamlit.error("Please select a fruit to get information.")
 else:
@@ -33,7 +33,7 @@ else:
  streamlit.dataframe(fruityvice_normalized)
  
 except URLError as e:
-    streamlit.error(e)
+    streamlit.error()
  
 #dont run anything past here while we troubleshoot
 streamlit.stop()
